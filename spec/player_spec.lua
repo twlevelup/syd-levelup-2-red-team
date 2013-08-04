2ad76c257340b4480207df2cedccebb2f4d01bde
Sam Gibson <sam@ifdown.net>
2013-08-04 14:13:20 +1000

    Spec for updating the animation state

--- spec/player_spec.lua
+++ spec/player_spec.lua
@@ -48,7 +48,7 @@ describe("Player", function()
         it("should decrement the player's y if the up-arrow is pressed", function()
             player = Player(mock_input('up'))
             local orig_y = player.y
-            player.update(0)
+            player.update(1)
 
             assert.is.equal(orig_y - player.speed, player.y)
         end)
@@ -56,7 +56,7 @@ describe("Player", function()
         it("should increment the player's y if the down-arrow is pressed", function()
             player = Player(mock_input('down'))
             local orig_y = player.y
-            player.update(0)
+            player.update(1)
 
             assert.is.equal(orig_y + player.speed, player.y)
         end)
@@ -64,7 +64,7 @@ describe("Player", function()
         it("should decrement the player's x if the left-arrow is pressed", function()
             player = Player(mock_input('left'))
             local orig_x = player.x
-            player.update(0)
+            player.update(1)
 
             assert.is.equal(orig_x - player.speed, player.x)
         end)
@@ -72,9 +72,20 @@ describe("Player", function()
         it("should increment the player's x if the right-arrow is pressed", function()
             player = Player(mock_input('right'))
             local orig_x = player.x
-            player.update(0)
+            player.update(1)
 
             assert.is.equal(orig_x + player.speed, player.x)
+        end)
+
+        it("should update the animation state", function()
+            player = Player(mock_input('none'))
+            local animation_spy = { update = spy.new(function(dt) end) }
+
+            player.graphics.animation = animation_spy
+
+            player.update(1)
+
+            assert.spy(animation_spy.update).was.called()
         end)
     end)
 end)
