2f0b3f41fd81dac5b89f0547cda760781e401884
James Gregory <james@jagregory.com>
2013-08-13 18:45:57 +1000

    Converted player to classy style.

--- player.lua
+++ player.lua
@@ -1,7 +1,10 @@
 require 'input'
 
-function Player(game)
-    local self = {
+Player = {}
+
+function Player:new(game)
+    newObj = {
+        game = game,
         size = 20,
         x = 400,
         y = 300,
@@ -18,76 +21,77 @@ function Player(game)
     }
 
     if game.audio ~= nil then
-        self.sound.moving.sample = game.audio.newSource(self.sound.moving.source)
-        self.sound.moving.sample:setLooping(true)
+        newObj.sound.moving.sample = game.audio.newSource(newObj.sound.moving.source)
+        newObj.sound.moving.sample:setLooping(true)
     end
 
     if game.graphics ~= nil and game.animation ~= nil then
-        self.graphics.sprites = game.graphics.newImage(self.graphics.source)
-        self.graphics.grid = game.animation.newGrid(
+        newObj.graphics.sprites = game.graphics.newImage(newObj.graphics.source)
+        newObj.graphics.grid = game.animation.newGrid(
             100, 70,
-            self.graphics.sprites:getWidth(),
-            self.graphics.sprites:getHeight()
+            newObj.graphics.sprites:getWidth(),
+            newObj.graphics.sprites:getHeight()
         )
-        self.graphics.animation = game.animation.newAnimation(
-            self.graphics.grid("1-6", 1),
+        newObj.graphics.animation = game.animation.newAnimation(
+            newObj.graphics.grid("1-6", 1),
             0.05
         )
     end
 
-    function self.draw()
-        self.graphics.animation:draw(self.graphics.sprites, self.x, self.y)
-    end
+    self.__index = self
+    return setmetatable(newObj, self)
+end
 
-    function self.update(dt)
-        local dx = 0
-        local dy = 0
+function Player:draw()
+    self.graphics.animation:draw(self.graphics.sprites, self.x, self.y)
+end
 
-        if game.input.pressed("left") then
-            dx = dx - self.speed
+function Player:update(dt)
+    local dx = 0
+    local dy = 0
 
-            if self.graphics.facing ~= "left" then
-                self.graphics.animation:flipH()
-                self.graphics.facing = "left"
-            end
-        end
+    if self.game.input.pressed("left") then
+        dx = dx - self.speed
 
-        if game.input.pressed("right") then
-            dx = dx + self.speed
-
-            if self.graphics.facing ~= "right" then
-                self.graphics.animation:flipH()
-                self.graphics.facing = "right"
-            end
-        end
-
-        if game.input.pressed("up") then
-            dy = dy - self.speed
-        end
-
-        if game.input.pressed("down") then
-            dy = dy + self.speed
-        end
-
-        self.x = self.x + dx
-        self.y = self.y + dy
-
-        if self.graphics.animation ~= nil then
-            if dx ~= 0 or dy ~= 0 then
-                self.graphics.animation:update(dt)
-            else
-                self.graphics.animation:gotoFrame(1)
-            end
-        end
-
-        if self.sound.moving.sample ~= nil then
-            if dx ~= 0 or dy ~= 0 then
-                self.sound.moving.sample:play()
-            else
-                self.sound.moving.sample:stop()
-            end
+        if self.graphics.facing ~= "left" then
+            self.graphics.animation:flipH()
+            self.graphics.facing = "left"
         end
     end
 
-    return self
+    if self.game.input.pressed("right") then
+        dx = dx + self.speed
+
+        if self.graphics.facing ~= "right" then
+            self.graphics.animation:flipH()
+            self.graphics.facing = "right"
+        end
+    end
+
+    if self.game.input.pressed("up") then
+        dy = dy - self.speed
+    end
+
+    if self.game.input.pressed("down") then
+        dy = dy + self.speed
+    end
+
+    self.x = self.x + dx
+    self.y = self.y + dy
+
+    if self.graphics.animation ~= nil then
+        if dx ~= 0 or dy ~= 0 then
+            self.graphics.animation:update(dt)
+        else
+            self.graphics.animation:gotoFrame(1)
+        end
+    end
+
+    if self.sound.moving.sample ~= nil then
+        if dx ~= 0 or dy ~= 0 then
+            self.sound.moving.sample:play()
+        else
+            self.sound.moving.sample:stop()
+        end
+    end
 end
--- spec/player_spec.lua
+++ spec/player_spec.lua
@@ -39,17 +39,17 @@ describe("Player", function()
 
         describe("playing the movement sound", function()
             it("should play the movement sound when the player is moving", function()
-                player = Player(mock_input('up'))
+                player = Player:new(mock_input('up'))
                 player.sound.moving.sample = mock_sound()
-                player.update(1)
+                player:update(1)
 
                 assert.spy(player.sound.moving.sample.play).was.called()
             end)
 
             it("should stop playing the movement sound when the player is stationary", function()
-                player = Player(mock_input('none'))
+                player = Player:new(mock_input('none'))
                 player.sound.moving.sample = mock_sound()
-                player.update(1)
+                player:update(1)
 
                 assert.spy(player.sound.moving.sample.stop).was.called()
             end)
@@ -58,24 +58,24 @@ describe("Player", function()
         describe("animating the player", function()
             describe("the sprite direction", function()
                 it("should point to the right by default", function()
-                    player = Player(mock_input('none'))
+                    player = Player:new(mock_input('none'))
                     assert.is.equal(player.graphics.facing, "right")
                 end)
 
                 it("should point to the right when the right arrow is pressed", function()
-                    player = Player(mock_input('right'))
+                    player = Player:new(mock_input('right'))
                     player.graphics.facing = "left"
                     player.graphics.animation = mock_animation()
-                    player.update(1)
+                    player:update(1)
 
                     assert.is.equal(player.graphics.facing, "right")
                     assert.spy(player.graphics.animation.flipH).was.called()
                 end)
 
                 it("should point to the left when the left arrow is pressed", function()
-                    player = Player(mock_input('left'))
+                    player = Player:new(mock_input('left'))
                     player.graphics.animation = mock_animation()
-                    player.update(1)
+                    player:update(1)
 
                     assert.is.equal(player.graphics.facing, "left")
                     assert.spy(player.graphics.animation.flipH).was.called()
@@ -84,19 +84,19 @@ describe("Player", function()
 
             describe("the animation frame", function()
                 it("should stop updating when the player isn't moving", function()
-                    player = Player(mock_input('none'))
+                    player = Player:new(mock_input('none'))
                     player.graphics.animation = mock_animation()
-                    player.update(1)
+                    player:update(1)
 
                     assert.spy(player.graphics.animation.gotoFrame).was.called()
                     assert.spy(player.graphics.animation.update).was_not.called()
                 end)
 
                 it("should update the animation state when the player is moving", function()
-                    player = Player(mock_input('up'))
+                    player = Player:new(mock_input('up'))
                     player.graphics.animation = mock_animation()
 
-                    player.update(1)
+                    player:update(1)
 
                     assert.spy(player.graphics.animation.update).was.called()
                 end)
@@ -104,34 +104,34 @@ describe("Player", function()
         end)
 
         it("should decrement the player's y if the up-arrow is pressed", function()
-            player = Player(mock_input('up'))
+            player = Player:new(mock_input('up'))
             local orig_y = player.y
-            player.update(1)
+            player:update(1)
 
             assert.is.equal(orig_y - player.speed, player.y)
         end)
 
         it("should increment the player's y if the down-arrow is pressed", function()
-            player = Player(mock_input('down'))
+            player = Player:new(mock_input('down'))
             local orig_y = player.y
-            player.update(1)
+            player:update(1)
 
             assert.is.equal(orig_y + player.speed, player.y)
         end)
 
         it("should decrement the player's x if the left-arrow is pressed", function()
-            player = Player(mock_input('left'))
+            player = Player:new(mock_input('left'))
             player.graphics.animation = mock_animation()
             local orig_x = player.x
-            player.update(1)
+            player:update(1)
 
             assert.is.equal(orig_x - player.speed, player.x)
         end)
 
         it("should increment the player's x if the right-arrow is pressed", function()
-            player = Player(mock_input('right'))
+            player = Player:new(mock_input('right'))
             local orig_x = player.x
-            player.update(1)
+            player:update(1)
 
             assert.is.equal(orig_x + player.speed, player.x)
         end)
