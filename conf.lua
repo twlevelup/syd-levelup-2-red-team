e645ff1d2ea577b8386ab1989e742185624ea466
Andrew Jones <andrew@andrewjones.id.au>
2013-08-18 16:15:43 +1000

    Disable love modules that we don't need

--- conf.lua
+++ conf.lua
@@ -1,3 +1,9 @@
+-- See https://love2d.org/wiki/Config_Files for the full list of configuration
+-- options and defaults.
+
 function love.conf(config)
     config.title = "Nyancat to the rescue"
+    config.modules.joystick = false
+    config.modules.mouse = false
+    config.modules.physics = false
 end
