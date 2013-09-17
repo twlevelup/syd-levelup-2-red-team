613481fa5a13a2cd6ad0dd4668051a2940e557ea
Scarecrow <turtles@redteam.tw>
2013-09-17 20:16:41 +1000

    R+S Added inner walls to the map.

--- main.lua
+++ main.lua
@@ -1,16 +1,22 @@
 require 'input'
 require 'player'
 require 'obstacle'
+require 'inner_wall'
 
 love.animation = require 'vendor/anim8'
 
 local entities = {}
 local player = Player:new(love, {x = 0, y = 0})
-local obstacle = Obstacle:new(love, {x = 200, y = 200})
+local wall_1 = InnerWall:new(love, {x = 100, y = 200})
+local wall_2 = InnerWall:new(love, {x = 100, y = 400})
+local wall_3 = InnerWall:new(love, {x = 100, y = 600})
 
 function love.load()
     table.insert(entities, player)
     table.insert(entities, obstacle)
+    table.insert(entities, wall_1)
+    table.insert(entities, wall_2)
+    table.insert(entities, wall_3)
 
     love.input.bind('up', 'up')
     love.input.bind('left', 'left')
