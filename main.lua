91bf9f5806bcba7e823fb6be1cae06d79392d013
Andrew Jones <andrew@andrewjones.id.au>
2013-09-24 20:38:35 +1000

    Fix the fruit that is stuck in the wall

--- main.lua
+++ main.lua
@@ -17,16 +17,16 @@ local wall_3 = InnerWall:new(love, {x = 200, y = 600})
 local MAPSCALE_X = 128;
 local MAPSCALE_Y = 96;
 
-local fruit_at_grid_10 = Fruit:new(love, {x = 1*MAPSCALE_X, y = 0*MAPSCALE_Y}) 
+local fruit_at_grid_10 = Fruit:new(love, {x = 1*MAPSCALE_X, y = 0*MAPSCALE_Y})
 local fruit_at_grid_70 = Fruit:new(love, {x = 7*MAPSCALE_X, y = 0*MAPSCALE_Y})
-local fruit_at_grid_51 = Fruit:new(love, {x = 5*MAPSCALE_X, y = 1*MAPSCALE_Y}) 
-local fruit_at_grid_33 = Fruit:new(love, {x = 3*MAPSCALE_X, y = 3*MAPSCALE_Y}) 
-local fruit_at_grid_23 = Fruit:new(love, {x = 2*MAPSCALE_X, y = 3*MAPSCALE_Y}) 
-local fruit_at_grid_64 = Fruit:new(love, {x = 6*MAPSCALE_X, y = 4*MAPSCALE_Y}) 
-local fruit_at_grid_05 = Fruit:new(love, {x = 0*MAPSCALE_X, y = 5*MAPSCALE_Y}) 
-local fruit_at_grid_45 = Fruit:new(love, {x = 4*MAPSCALE_X, y = 5*MAPSCALE_Y}) 
-local fruit_at_grid_37 = Fruit:new(love, {x = 3*MAPSCALE_X, y = 7*MAPSCALE_Y}) 
-local fruit_at_grid_77 = Fruit:new(love, {x = 7*MAPSCALE_X, y = 7*MAPSCALE_Y}) 
+local fruit_at_grid_51 = Fruit:new(love, {x = 5*MAPSCALE_X, y = 1*MAPSCALE_Y})
+local fruit_at_grid_33 = Fruit:new(love, {x = 3*MAPSCALE_X, y = 3*MAPSCALE_Y})
+local fruit_at_grid_23 = Fruit:new(love, {x = 2*MAPSCALE_X, y = 3*MAPSCALE_Y})
+local fruit_at_grid_64 = Fruit:new(love, {x = 7*MAPSCALE_X, y = 3*MAPSCALE_Y})
+local fruit_at_grid_05 = Fruit:new(love, {x = 0*MAPSCALE_X, y = 5*MAPSCALE_Y})
+local fruit_at_grid_45 = Fruit:new(love, {x = 4*MAPSCALE_X, y = 5*MAPSCALE_Y})
+local fruit_at_grid_37 = Fruit:new(love, {x = 3*MAPSCALE_X, y = 7*MAPSCALE_Y})
+local fruit_at_grid_77 = Fruit:new(love, {x = 7*MAPSCALE_X, y = 7*MAPSCALE_Y})
 
 
 -- Outer Walls render all 4 render
@@ -42,21 +42,19 @@ function love.load()
     table.insert(entities, fruit_at_grid_10)
     table.insert(entities, fruit_at_grid_70)
     table.insert(entities, fruit_at_grid_51)
-    table.insert(entities, fruit_at_grid_33) 
+    table.insert(entities, fruit_at_grid_33)
     table.insert(entities, fruit_at_grid_23)
-    table.insert(entities, fruit_at_grid_64) 
+    table.insert(entities, fruit_at_grid_64)
     table.insert(entities, fruit_at_grid_05)
     table.insert(entities, fruit_at_grid_45)
     table.insert(entities, fruit_at_grid_37)
     table.insert(entities, fruit_at_grid_77)
 
 
-    -- Loop insert OuterWalls, "#<tablename>" returns number of elements in table
     for i = 0, #outerWalls do
         table.insert(entities, outerWalls[i])
     end
 
-    -- Bind keystroke commands
     love.input.bind('up', 'up')
     love.input.bind('left', 'left')
     love.input.bind('right', 'right')
