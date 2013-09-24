b7afe5fc8dcb4af4bc1a1e69ff63c6c63e2fa806
red <red@levelup.com>
2013-09-24 20:30:14 +1000

    C+S Repositioned onscreen fruit.

--- main.lua
+++ main.lua
@@ -13,20 +13,40 @@ local wall_1 = InnerWall:new(love, {x = 200, y = 200})
 local wall_2 = InnerWall:new(love, {x = 200, y = 400})
 local wall_3 = InnerWall:new(love, {x = 200, y = 600})
 
-
+-- Fruits
 local MAPSCALE_X = 128;
 local MAPSCALE_Y = 96;
 
-local fruit_at_grid_10 = Fruit:new(love, {x = 1*MAPSCALE_X, y = 0*MAPSCALE_Y})
-local fruit_at_grid_70 = Fruit:new(love, {x = 7*MAPSCALE_X, y = 0*MAPSCALE_Y})
-local fruit_at_grid_51 = Fruit:new(love, {x = 5*MAPSCALE_X, y = 1*MAPSCALE_Y})
-local fruit_at_grid_33 = Fruit:new(love, {x = 3*MAPSCALE_X, y = 3*MAPSCALE_Y})
-local fruit_at_grid_23 = Fruit:new(love, {x = 2*MAPSCALE_X, y = 3*MAPSCALE_Y})
-local fruit_at_grid_64 = Fruit:new(love, {x = 7*MAPSCALE_X, y = 3*MAPSCALE_Y})
-local fruit_at_grid_05 = Fruit:new(love, {x = 0*MAPSCALE_X, y = 5*MAPSCALE_Y})
-local fruit_at_grid_45 = Fruit:new(love, {x = 4*MAPSCALE_X, y = 5*MAPSCALE_Y})
-local fruit_at_grid_37 = Fruit:new(love, {x = 3*MAPSCALE_X, y = 7*MAPSCALE_Y})
-local fruit_at_grid_77 = Fruit:new(love, {x = 7*MAPSCALE_X, y = 7*MAPSCALE_Y})
+fruits = {}
+
+-- First Row Fruits
+local fruit_at_grid_11 = Fruit:new(love, {x = 1*MAPSCALE_X, y = 1*MAPSCALE_Y}) 
+local fruit_at_grid_41 = Fruit:new(love, {x = 4*MAPSCALE_X, y = 1*MAPSCALE_Y})
+local fruit_at_grid_71 = Fruit:new(love, {x = 7*MAPSCALE_X, y = 1*MAPSCALE_Y}) 
+
+-- Second Row Fruits
+local fruit_at_grid_33 = Fruit:new(love, {x = 3*MAPSCALE_X, y = 3*MAPSCALE_Y}) 
+local fruit_at_grid_53 = Fruit:new(love, {x = 5*MAPSCALE_X, y = 3*MAPSCALE_Y}) 
+
+-- Third Row Fruits
+local fruit_at_grid_15 = Fruit:new(love, {x = 1*MAPSCALE_X, y = 5*MAPSCALE_Y}) 
+local fruit_at_grid_45 = Fruit:new(love, {x = 4*MAPSCALE_X, y = 5*MAPSCALE_Y}) 
+local fruit_at_grid_75 = Fruit:new(love, {x = 7*MAPSCALE_X, y = 5*MAPSCALE_Y}) 
+
+-- Fourth Row Fruits
+local fruit_at_grid_37 = Fruit:new(love, {x = 3*MAPSCALE_X, y = 7*MAPSCALE_Y}) 
+local fruit_at_grid_57 = Fruit:new(love, {x = 5*MAPSCALE_X, y = 7*MAPSCALE_Y}) 
+
+table.insert(fruits, fruit_at_grid_11)
+table.insert(fruits, fruit_at_grid_41)
+table.insert(fruits, fruit_at_grid_71)
+table.insert(fruits, fruit_at_grid_33)
+table.insert(fruits, fruit_at_grid_53)
+table.insert(fruits, fruit_at_grid_15)
+table.insert(fruits, fruit_at_grid_45)
+table.insert(fruits, fruit_at_grid_75)
+table.insert(fruits, fruit_at_grid_37)
+table.insert(fruits, fruit_at_grid_57)
 
 
 -- Outer Walls render all 4 render
@@ -39,22 +59,17 @@ function love.load()
     table.insert(entities, wall_2)
     table.insert(entities, wall_3)
 
-    table.insert(entities, fruit_at_grid_10)
-    table.insert(entities, fruit_at_grid_70)
-    table.insert(entities, fruit_at_grid_51)
-    table.insert(entities, fruit_at_grid_33)
-    table.insert(entities, fruit_at_grid_23)
-    table.insert(entities, fruit_at_grid_64)
-    table.insert(entities, fruit_at_grid_05)
-    table.insert(entities, fruit_at_grid_45)
-    table.insert(entities, fruit_at_grid_37)
-    table.insert(entities, fruit_at_grid_77)
-
-
+    -- Loop insert OuterWalls, "#<tablename>" returns number of elements in table
     for i = 0, #outerWalls do
         table.insert(entities, outerWalls[i])
     end
 
+    -- Load fruits
+    for i = 0, #fruits do
+        table.insert(entities, fruits[i])
+    end
+
+    -- Bind keystroke commands
     love.input.bind('up', 'up')
     love.input.bind('left', 'left')
     love.input.bind('right', 'right')
