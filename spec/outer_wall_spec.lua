f4ff160005a42fe5cc2b1788a447026d0d9a8c6c
Andrew Jones <andrew@andrewjones.id.au>
2013-09-24 18:31:28 +1000

    Fix code layout in outerwall spec

--- spec/outer_wall_spec.lua
+++ spec/outer_wall_spec.lua
@@ -2,38 +2,38 @@ require 'outer_wall'
 require 'player'
 
 describe("OuterWall", function()
-	local dt = 1
+    local dt = 1
 
-	local screenX = 1024
-	local screenY = 768
+    local screenX = 1024
+    local screenY = 768
 
-	describe("new", function()
-		local left_outer_wall
+    describe("new", function()
+        local left_outer_wall
 
-		before_each(function()
-			left_outer_wall = OuterWall:new({})
-			end)
+        before_each(function()
+            left_outer_wall = OuterWall:new({})
+        end)
 
-		it("should have x value of 0 by default", function()
-			assert.is.equal(0, left_outer_wall.x)
-			end)
+        it("should have x value of 0 by default", function()
+            assert.is.equal(0, left_outer_wall.x)
+        end)
 
-		it("should have y value of 0 by default", function()
-			assert.is.equal(0, left_outer_wall.y)
-			end)
+        it("should have y value of 0 by default", function()
+            assert.is.equal(0, left_outer_wall.y)
+        end)
 
-		it("should have size x=1, y=768 by default", function()
-			local expected = { x = 1, y = screenY }
-			assert.are.same(expected, left_outer_wall.size)
-			end)
+        it("should have size x=1, y=768 by default", function()
+            local expected = { x = 1, y = screenY }
+            assert.are.same(expected, left_outer_wall.size)
+        end)
 
-		end)
+    end)
 
-	describe("Player", function()
-		local player
-		local left_outer_wall
+    describe("Player", function()
+        local player
+        local left_outer_wall
 
-		mock_input = function(action)
+        mock_input = function(action)
             return {
                 input = {
                     pressed = function(a)
@@ -66,45 +66,43 @@ describe("OuterWall", function()
             return sound_spy
         end
 
-		before_each(function()
-			player = Player:new({})
-			left_outer_wall = OuterWall:new({})
-			end)
+        before_each(function()
+            player = Player:new({})
+            left_outer_wall = OuterWall:new({})
+        end)
 
-		it("should not be able to move left when at x=1 y=400", function()
-			orig_x = 1
-			orig_y = 400
-			local player = Player:new(
-				mock_input('left'),
-				{
-					x = orig_x,
-					y = orig_y,
-					speed = 1
-				}
-			)
-			player.graphics.animation = mock_animation()
-			player:update(dt)
+        it("should not be able to move left when at x=1 y=400", function()
+            orig_x = 1
+            orig_y = 400
+            local player = Player:new(
+            mock_input('left'),
+            {
+                x = orig_x,
+                y = orig_y,
+                speed = 1
+            }
+            )
+            player.graphics.animation = mock_animation()
+            player:update(dt)
 
-			local expectedSize = {x = 1, y = screenY}
+            local expectedSize = {x = 1, y = screenY}
 
-			-- Check to see that the outer wall exists
-			assert.is.equal(0, left_outer_wall.x)
-			assert.is.equal(0, left_outer_wall.y)
-			assert.are.same(expectedSize, left_outer_wall.size)
-			assert.is.equal("left", left_outer_wall.position)
+            -- Check to see that the outer wall exists
+            assert.is.equal(0, left_outer_wall.x)
+            assert.is.equal(0, left_outer_wall.y)
+            assert.are.same(expectedSize, left_outer_wall.size)
+            assert.is.equal("left", left_outer_wall.position)
 
 
-			assert.are.same({x = orig_x, y = orig_y}, player.lastPosition) -- Last position hasn't changed.
-			assert.is.equal(400, player.y) -- Still at y = 400
+            assert.are.same({x = orig_x, y = orig_y}, player.lastPosition) -- Last position hasn't changed.
+            assert.is.equal(400, player.y) -- Still at y = 400
 
-			assert.is.equal(true, player:collidingWith(left_outer_wall)) -- Registers that it's colliding
+            assert.is.equal(true, player:collidingWith(left_outer_wall)) -- Registers that it's colliding
 
-			-- assert.is.equal(1, player.x) -- WHY YOU NO STOP AT WALL...?
-			-- Actual (Collide) function is called in main.lua and not part of the model.. Maybe that's way.
-			-- TODO: Ask Andrew about this.
+            -- assert.is.equal(1, player.x) -- WHY YOU NO STOP AT WALL...?
+            -- Actual (Collide) function is called in main.lua and not part of the model.. Maybe that's way.
+            -- TODO: Ask Andrew about this.
 
-		end)
-
-	end)
-
+        end)
+    end)
 end)
