7709a4877385399cfa7d71d8cd0d657cc681585c
Chris James <chris@webglowit.net>
2013-09-25 13:07:22 +1000

    MOA TESTS :D

--- spec/fruit_spec.lua
+++ spec/fruit_spec.lua
@@ -14,12 +14,20 @@ describe("Fruit", function()
 
 			local fruit2 = Fruit:randomlyPlace({}, fruits, {}, 1)
 
-			assert.is_not.equal(fruits[1].x, fruit2[1].x)
+			assert.is_not.equal({fruit1[1].x, fruit1[1].y}, {fruit2[1].x, fruit2[1].y})
 
 		end)
 
 		it("should be able to create 5 fruits without repeating", function()
 
+			local fruit5 = Fruit:randomlyPlace({}, fruits, {}, 5)
+			for _, fruitA in pairs(fruits) do
+				for _, fruitB in pairs(fruits) do
+					if fruitA ~= fruitB then
+						assert.is_not.equal({fruitA.x, fruitB.y}, {fruitB.x, fruitB.y})
+					end
+				end
+			end
 
 		end)
 
