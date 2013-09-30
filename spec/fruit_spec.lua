326fbe2d091ee59c8c52875adce46b4bd69b0c32
Chris James <chris@webglowit.net>
2013-09-30 23:51:38 +1000

    Test to unsure fruits don't collide with walls

--- spec/fruit_spec.lua
+++ spec/fruit_spec.lua
@@ -18,13 +18,67 @@ describe("Fruit", function()
 
 		end)
 
-		it("should be able to create 5 fruits without repeating", function()
+		it("should be able to create 5 fruits in 5 different positions", function()
 
 			local fruit5 = Fruit:randomlyPlace({}, fruits, {}, 5)
 			for _, fruitA in pairs(fruits) do
 				for _, fruitB in pairs(fruits) do
 					if fruitA ~= fruitB then
-						assert.is_not.equal({fruitA.x, fruitB.y}, {fruitB.x, fruitB.y})
+						assert.is_not.equal({fruitA.x, fruitA.y}, {fruitB.x, fruitB.y})
+					end
+				end
+			end
+
+		end)
+
+		it("should be able to create fruit which don't collide with walls", function() 
+
+			entities = {}
+
+			wallConfig1 = {
+				x = 60,
+				y = 60,
+				size = {
+					x = 500,
+					y = 50
+				}
+			}
+
+			wall1 = InnerWall:new(wallConfig1)
+
+			wallConfig2 = {
+				x = 60,
+				y = 170,
+				size = {
+					x = 600,
+					y = 50
+				}
+			}
+
+			wall2 = InnerWall:new(wallConfig2)
+
+
+			wallConfig3 = {
+				x = 60,
+				y = 350,
+				size = {
+					x = 400,
+					y = 70
+				}
+			}
+
+			wall3 = InnerWall:new(wallConfig3)
+
+			table.insert(entities, wall1)
+			table.insert(entities, wall2)
+			table.insert(entities, wall3)
+
+			local fruits = Fruit:randomlyPlace({}, entities, {}, 5)
+
+			for _, fruit in pairs(fruits) do
+				for _, entity in pairs(entities) do
+					if fruit ~= entity then
+						assert.is_false(fruit:collidingWith(entity))
 					end
 				end
 			end
