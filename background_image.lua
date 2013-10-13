dd07886aa8aff64c6becb5e606dd29251c3ce98a
Chris James <chris@webglowit.net>
2013-10-14 03:35:21 +1100

    Rewrite of BG image. No longer using entity class based images and animations. Repeat X and Y, and draw.

--- background_image.lua
+++ background_image.lua
@@ -3,46 +3,68 @@ require 'presets'
 BackgroundImage = {}
 BackgroundImage.__index = BackgroundImage
 
-function BackgroundImage:new()
-	
+function BackgroundImage:new(game)
 
-	local config = config or {}
-
-    local newBackgroundImage = Entity:new(game)
-    newBackgroundImage.type = "BackgroundImage"
-    newBackgroundImage.x = config.x or 0
-    newBackgroundImage.y = config.y or 0
-    newBackgroundImage.size = config.size or {
-        x = GAME_WIDTH,
-        y = GAME_HEIGHT
+    newBackgroundImage = {
+        fenceTopImage = game.graphics.newImage("assets/images/fence-top.png"),
+        fenceBotImage = game.graphics.newImage("assets/images/fence-bottom.png"),
+        grassImage = game.graphics.newImage("assets/images/grass.png"),
+        soilImage = game.graphics.newImage("assets/images/soil.png"),
+        love = game
     }
-
-    newBackgroundImage.graphics = config.graphics or {
-        source = "assets/images/bg.png"
-    }
-
-    if game.graphics ~= nil and game.animation ~= nil then
-        newBackgroundImage.graphics.sprites = game.graphics.newImage(newBackgroundImage.graphics.source)
-        newBackgroundImage.graphics.grid = game.animation.newGrid(
-            newBackgroundImage.size.x, newBackgroundImage.size.y,
-            newBackgroundImage.graphics.sprites:getWidth(),
-            newBackgroundImage.graphics.sprites:getHeight()
-        )
-        newBackgroundImage.graphics.animation = game.animation.newAnimation(
-            newBackgroundImage.graphics.grid("1-1", 1),
-            0.5
-        )
-    end
 
 	return setmetatable(newBackgroundImage, self)
 end
 
---Need to possibly implement a gaurd against score going into negative, check with BA
-function BackgroundImage:update(value)
-	self.score = value + self.score
+
+function BackgroundImage:calculateRepeat()
+
+    local repeats = {
+        x = 0,
+        y = 0
+    }
+
+    local sizeX = self.fenceTopImage:getWidth()
+    local sizeY = self.fenceTopImage:getHeight()
+    local screenHeight = self.love.graphics.getHeight()
+    local screenWidth = self.love.graphics.getWidth()
+
+    repeats.x = math.ceil(screenWidth/sizeX)
+    repeats.y = math.ceil(screenHeight/sizeY)
+
+    return repeats
 end
 
-function BackgroundImage:draw()
-    love.graphics.print(self.score, self.x_pos, self.y_pos, self.orientation, 
-    	self.x_scale, self.y_scale)
+function BackgroundImage:repeatImages(repeats)
+
+    local x = repeats.x
+    local y = repeats.y
+
+    local i, j
+
+    for i = 0,y do
+        for j = 0,x do
+
+            if i == 0 then
+                self:draw(self.grassImage, j, i)
+            elseif i == 1 then
+                self:draw(self.fenceTopImage, j, i)
+            else
+                self:draw(self.soilImage, j, i)
+            end    
+        end
+    end
+
 end
+
+function BackgroundImage:draw(tile, offsetX, offsetY)
+    love.graphics.draw(tile, 0 + (40*offsetX), 0 + (40*offsetY), 0, 1, 1, 0, 0)
+end
+
+function BackgroundImage:fillBackground()
+
+    local repeats = self:calculateRepeat()
+    self:repeatImages(repeats)
+
+end
+
