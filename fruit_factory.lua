0fbaf24dfe1205f315217a0b35f9b5d9063a2ce9
Chris James <chris@webglowit.net>
2013-10-14 04:25:52 +1100

    Change the fruit render logic, now to render within playing area, with a safety buffer.. Now improved accuracy. Still a small bug

--- fruit_factory.lua
+++ fruit_factory.lua
@@ -17,8 +17,8 @@ function FruitFactory:createRandomFruit(game)
     local config = config or {}
     local game = game or {}
 
-    config.x = math.random(1024)
-    config.y = math.random(768) + GAME_INFO_OFFSET_Y
+    config.x = math.random(game.graphics.getWidth() - 2*GAME_FENCE_BUFFER) + GAME_FENCE_BUFFER
+    config.y = math.random(game.graphics.getHeight() - GAME_INFO_OFFSET_Y - GAME_FRUIT_SIDE_LENGTH - 2*GAME_FENCE_BUFFER) + GAME_INFO_OFFSET_Y +GAME_FENCE_BUFFER
     local fruit = Fruit:new(game, config)
 
 	return fruit
